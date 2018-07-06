ae991422c33a7c535d5f6e2811511b9d03a1dd98
Jack Briones <jbriones@caltech.edu>
2018-07-06 00:42:45 -0700

    added more preprocessing, and implemented a few more extraction methods. Check the TODOs to see the remaining bugs

--- grabber.py
+++ grabber.py
@@ -2,44 +2,114 @@ import codecs
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
-
-
-
-# Do precomputing on the text.
-# TODO: consider the following:
-#           how should we handle punctuation?
+import re
 
 #==============================================================================#
 # File I/O
 #==============================================================================#
-# Note: the input file should just be a plaintext. Possibly use a different
-#       encoding.
-file_in = codecs.open("twitter_as_data.txt", "r", encoding="utf-8")
+# NOTE: text should be in unicode format. Depending on our sources, I'll look
+#       into format conversions.
+
+file_name = raw_input("Enter file name: ")
+if not file_name: file_name = "twitter_as_data.txt"
+
+file_in = codecs.open(file_name, "r", encoding="utf-8")
 text = file_in.read()
 sents = sent_tokenize(text)
+
+# remove hashtags, URLs, and mentions here
+#               Note: these can easily be condensed into one regular expression,
+#               but for now I will leave them separate for easy testing purposes
+mention_form = re.compile('@\\S+')
+url_form = re.compile('http\\S+')
+hashtag_form = re.compile('#\\S+')
+# https://stackoverflow.com/a/33417311
+# TODO: this doesn't work right now, I'm not sure why. will do some digging.
+# emoji_form = re.compile("["
+#         u"\U0001F600-\U0001F64F"  # emoticons
+#         u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+#         u"\U0001F680-\U0001F6FF"  # transport & map symbols
+#         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+#                            "]+", flags=re.UNICODE | re.VERBOSE)
+for i in range(len(sents)):
+    sents[i] = mention_form.sub("", sents[i])
+    sents[i] = hashtag_form.sub("", sents[i])
+    sents[i] = url_form.sub("", sents[i])
+#     sents[i] = emoji_form.sub("", sents[i])
+
 words = [word_tokenize(s) for s in sents]
 
-
-# remove stopwords here
+# Introduce default stopwords
 stop_words = set(stopwords.words('english'))
 
+# Let user define some of their own stop words
+print("Enter any additional stop words, one at a time. Press enter when you \
+are finished.")
+while (True):
+    new_word = raw_input("Add a stop word: ")
+    if not new_word:
+        break
+    stop_words.add(new_word)
+print 
+
+removal = set(['.', 'NUM'])
+
 for i in range(len(words)):
-    words[i] = [w for w in words[i] if not w in stop_words]
+    # Switch sentence to lower case
+    words[i] = [w.lower() for w in words[i]]
+    # Perform POS tagging
+    words[i] = nltk.pos_tag(words[i], tagset='universal')
+    # Remove stop words, punctuation, and numbers
+    # TODO: fix punctuation. it seems like the unicode apostrophe gets through
+    #       the filter, even though the ' character is successfully removed.
+    words[i] = [w for w in words[i] if not w[0] in stop_words
+                and not w[1] in removal]
 
-# Sort the text into bigrams, one sentence at a time.
-bgrms = []
+# Sort the text into n-grams, one sentence at a time.
+ngrams = []
 for sentence in words:
-    bgrms += list(nltk.bigrams(sentence))
+    # unigrams, bigrams, and trigrams - change max_len to get more or less.
+    ngrams += list(nltk.everygrams(sentence, max_len = 3))
 
-# Get the frequency distribution of the bigrams
-bg_fd = nltk.FreqDist(bgrms)
+# ngrams includes all types of n-grams. get lists for each type as well.
+unigrams = [n for n in ngrams if len(n) == 1]
+bigrams = [n for n in ngrams if len(n) == 2]
+trigrams = [n for n in ngrams if len(n) == 3]
 
-# Now, store the entries from bg_fd.
+# Get the frequency distribution of each
+ngrams_fd = nltk.FreqDist(ngrams)
+unigrams_fd = nltk.FreqDist(unigrams)
+bigrams_fd = nltk.FreqDist(bigrams)
+trigrams_fd = nltk.FreqDist(trigrams)
+
+# set the number of keywords to extract from each method
+num_kw = 10
+
+# Get the most common of each:
+print str(num_kw) + " most common unigrams:"
+for w in unigrams_fd.most_common(num_kw):
+    print w[0][0][0]
+
+print "\n" + str(num_kw) + " most common nouns:"
+nouns = [w for w in unigrams if w[0][1] == 'NOUN']
+nouns_fd = nltk.FreqDist(nouns)
+for w in nouns_fd.most_common(num_kw):
+    print w[0][0][0]
+
+print "\n" + str(num_kw) + " most common bigrams:"
+for w in bigrams_fd.most_common(num_kw):
+    print w[0][0][0] + " " + w[0][1][0]
+
+print "\n" + str(num_kw) + " most common trigrams:"
+for w in trigrams_fd.most_common(num_kw):
+    print w[0][0][0] + " " + w[0][1][0] + " " + w[0][2][0]
+
+# Now, store the entries from bigrams_fd to compute selectivity results.
 degree = {}
 strength = {}
 
 # current form: co-occurrence, weighted and undirected
-most_common = bg_fd.most_common()
+most_common = bigrams_fd.most_common()
 for ((a, b), c) in most_common:
     if not a in degree.keys():
         degree[a] = 0
@@ -61,26 +131,9 @@ for w in degree.keys():
 
 # Result extraction:
 
-# num_kw = ___          (number of keywords to select)
-# selectivity.sort()
-# selectivity.reverse()
-# results = [____ for (n, s) in selectivity[:num_kw]]
+print "\n" + str(num_kw) + " best selectivity scores:"
+selectivity.sort()
+selectivity.reverse()
+for (n, s) in selectivity[:num_kw]:
+    print s[0] + " : " + str(n)
 
-#==============================================================================#
-# POS tagging
-#==============================================================================#
-
-text_in = []
-for sent in words:
-    text_in += sent
-
-tagged_text = nltk.pos_tag(text_in, tagset='universal')
-# Only save nouns for now. This may change.
-save_words = ["NOUN"]
-tagged_text = [t for (t, u) in tagged_text if u in save_words]
-tagged_fd = nltk.FreqDist(tagged_text)
-
-# Result extraction:
-
-# num_kw = ___          (number of keywords to select)
-# results = [____ for (w, n) in tagged_fd.most_common(num_kw)]
